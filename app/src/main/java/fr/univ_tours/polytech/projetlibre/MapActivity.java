d68d9e681196923fcbd3299280a949116ff397ca
alkpo1837 <monsieurjaky@gmail.com>
2017-02-13 14:09:54 +0100

    The user's localisation is now displayed; a circle is added at my place, and we can click on it

--- app/src/main/java/fr/univ_tours/polytech/projetlibre/MapActivity.java
+++ app/src/main/java/fr/univ_tours/polytech/projetlibre/MapActivity.java
@@ -1,5 +1,7 @@
 package fr.univ_tours.polytech.projetlibre;
 
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -8,6 +10,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -15,26 +20,43 @@ import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapActivity extends Fragment implements OnMapReadyCallback {
+public class MapActivity extends Fragment
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private MapView mMapView;
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+    private Circle circle;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View v = inflater.inflate(R.layout.fragment_map, container, false);
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
 
         mMapView = (MapView) v.findViewById(R.id.mapView);
+
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();
 
         mMapView.getMapAsync(this);
+
+        if (mGoogleApiClient == null)
+        {
+            mGoogleApiClient = new GoogleApiClient.Builder(this.getContext())
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         return v;
     }
@@ -44,10 +66,58 @@ public class MapActivity extends Fragment implements OnMapReadyCallback {
     {
         mMap = googleMap;
 
-        LatLng sydney = new LatLng(47.3945427, 0.6910287000000608);
+        MapsInitializer.initialize(getActivity());
 
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Chez DJ"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMapClickListener(this);
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(47.3945427, 0.6910287000000608))
+                .radius(100)
+                .strokeWidth(0.0f)
+                .fillColor(0x750000ff);
+
+
+         circle = mMap.addCircle(circleOptions);
     }
 
+    @Override
+    public void onMapClick(LatLng position) {
+
+        LatLng center = circle.getCenter();
+        double radius = circle.getRadius();
+        float[] distance = new float[1];
+        Location.distanceBetween(position.latitude, position.longitude, center.latitude, center.longitude, distance);
+        boolean clicked = distance[0] < radius;
+
+        System.out.println("Cercle");
+    }
+
+    public void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    public void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()), 15.0f));
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
 }
