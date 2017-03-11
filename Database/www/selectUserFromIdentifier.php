7203e941c3d152ac6f3f7bb00e3ff608e2c233d3
quentin <quentin.combemorel@hotmail.fr>
2017-03-11 14:21:00 +0100

    Json Bug Fix

--- Database/www/selectUserFromIdentifier.php
+++ Database/www/selectUserFromIdentifier.php
@@ -6,7 +6,7 @@
         $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
         $bdd = new PDO('mysql:host=localhost;dbname=projetlibre', 'root', '', $pdo_options);
 
-		if (isset(($_POST['mail']) && isset($_POST['password'])))
+		if (isset($_POST['mail']) && isset($_POST['password']))
 		{	
 			$myMail = (string) $_POST['mail'];
 			$myPassword = (string) $_POST['password'];
@@ -14,8 +14,8 @@
 			$sql = 'SELECT * FROM user WHERE mail = ? and password = ?';
 			
 			$stmt = $bdd->prepare($sql);
-			$stmt->bindParam(1, $myMail, PDO::PARAM_STRING);
-			$stmt->bindParam(2, $myPassword, PDO::PARAM_STRING);
+			$stmt->bindParam(1, $myMail, PDO::PARAM_STR);
+			$stmt->bindParam(2, $myPassword, PDO::PARAM_STR);
 			$stmt->execute();
 			
 			$output = $stmt->fetch();
